2edf98e78115d2a90532141c6ffaf21e18b0eae3
Daiana Ghisio <daianaghisio@gmail.com>
2022-07-31 15:45:31 -0300

    Entrega BackEnd

---
+++ BackEnd/src/main/java/com/portfolio/dg/Controller/PersonaController.java
@@ -0,0 +1,63 @@
+package com.portfolio.dg.Controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.portfolio.dg.Entity.Persona;
+import com.portfolio.dg.Interface.PersonaService;
+
+@RestController
+@CrossOrigin(origins="http://localhost:4200")
+public class PersonaController {
+	@Autowired PersonaService personaService;
+	
+	
+	@GetMapping("personas/traer")
+	public List<Persona> getPersona(){
+		return personaService.getPersona();
+	}
+	
+	@PreAuthorize("hasRole('ADMIN')")
+	@PostMapping("/personas/crear")
+	public String createPersona(@RequestBody Persona persona) {
+		personaService.savePersona(persona);
+		return "La persona fue creada correctamente.";
+	}
+	
+	@PreAuthorize("hasRole('ADMIN')")
+	@DeleteMapping("/personas/borrar/{id}")
+	public String deletePersona(@PathVariable Long id) {
+		personaService.deletePersona(id);
+		return "La persona fue eliminada correctamente.";
+	}
+	
+	@PreAuthorize("hasRole('ADMIN')")
+	@PutMapping("/personas/editar/{id}")
+	public Persona editPersona(@PathVariable Long id, @RequestParam("nombre") String nuevoNombre, @RequestParam("apellido") String nuevoApellido, @RequestParam("img") String nuevaImg) {
+		Persona persona = personaService.findPersona(id);
+		persona.setNombre(nuevoNombre);
+		persona.setApellido(nuevoApellido);
+		persona.setImg(nuevaImg);
+		
+		personaService.savePersona(persona);
+		
+		return persona;
+	}
+	
+	@GetMapping("/personas/traer/perfil")
+	public Persona findPersona() {
+		return personaService.findPersona((long)1);  //Para que funcione el id en la bbdd tiene que ser 1
+	}
+	
+}
